eccf32fec1066560c5ad4338760fa90990bcce07
Alan <alan.andersonbr@gmail.com>
2022-08-13 23:57:43 -0300

    utilizando imagens da Api

--- app/src/main/java/com/example/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/com/example/soccernews/ui/adapter/NewsAdapter.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.soccernews.databinding.NewsItemBinding;
 import com.example.soccernews.domain.News;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -34,6 +35,8 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         News news = this.news.get(position);
         holder.binding.tvTitle.setText(news.getTitle());
         holder.binding.tvDescription.setText(news.getDescription());
+        Picasso.get().load(news.getImage()).into(holder.binding.imView);
+        gold
     }
 
     @Override
